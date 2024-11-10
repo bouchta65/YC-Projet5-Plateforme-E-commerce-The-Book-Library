b1425683f9736ecb902670fd022294882756209c
bouchta65 <bouchtamohamed01@gmail.com>
2024-11-10 23:08:30 +0100

    mise a jour le config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,17 @@ module.exports = {
   plugins: [],
 }
 
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customYellow: '#F9F073',
+        customblack: '#5D7285',
+        customblue: '#0C7FDA',
+      },
+    },
+  },
+  plugins: [],
+}
+
