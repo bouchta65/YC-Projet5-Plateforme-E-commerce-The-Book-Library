8a862537e9e29517001bbd4766b7c9ad6c6cab1f
bouchta65 <bouchtamohamed01@gmail.com>
2024-11-13 07:33:15 +0100

    Mise à jour de tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
+  content: ["./pages/**/*.{html,js}", "./scripts/**/*.{js}"],
+  safelist: [
+    'customYellow', 'customblack', 'customblue', // add any other needed classes
+  ],
   theme: {
     extend: {
       colors: {
         customYellow: '#F9F073',
         customblack: '#5D7285',
         customblue: '#0C7FDA',
-        customblaack : '#131313',
+        customblaack: '#131313',
         custemgray: '#727272',
-        custemgraytext : '#E2E6E9',
+        custemgraytext: '#E2E6E9',
+        custembleumenu: '#4B6587',
       },
     },
   },
   plugins: [],
-}
+};
