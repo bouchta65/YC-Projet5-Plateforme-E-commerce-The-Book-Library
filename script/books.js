4d5dd38cfc35f92a4e405720877fbe81abbd4374
Saad el quaul <saadelquaul@gmail.com>
2024-11-14 12:07:17 +0100

    fixing the sorting method and showing books on products section

--- script/books.js
+++ script/books.js
@@ -1,37 +1,56 @@
 
+const booksContainer = document.querySelector('.books-container');
+let booksData = []
 
-const api2 = "https://file-json-test.vercel.app/fichier.json"
+document.addEventListener("DOMContentLoaded", () => {
 
-async function booksAPI(){
-    const respond = await fetch(api2);
-    const data = await respond.json();
-    console.log(data);
+    booksData = JSON.parse(localStorage.getItem('booksData'));
+    displayBooks(booksData);
+
     
-    displayBooks(data);
-    }
-    booksAPI()
+});
+
     function displayBooks (data){
 
-        const booksToDisplay = data;
-        const booksContainer = document.querySelector('.books-container');
+       
+        booksContainer.innerHTML = "";
 
-        booksToDisplay.forEach(book => {
+        data.forEach(book => {
           let bookElement = document.createElement('div');
             bookElement.classList.add('book-item','w-auto','flex','flex-col','items-center','gap-1');
 
-            bookElement.innerHTML = `<div class="w-auto flex flex-col items-center gap-1 ">
-              <img src="${book.img}" alt="${book.title}">
+            bookElement.innerHTML = `<div class="w-auto h-[350px] flex flex-col items-center justify-between gap-1 ">
+              <div class="bg-[#4B6587] w-[200px] h-[230px] max-[750px]:w-[130px] ">
+              <a onclick="" href="../pages/details.html"><img src="${book.img}" alt="${book.title} " class="w-full h-full"></a>
+              </div>
               <h2 class="font-bold text-center">${book.title}</h2>
               <h3 class="font-extralight text-center">${book.author}</h3>
               <button type="button" id="${book.id}" class="bg-orange-600 font-bold text-white  h-[40px] w-auto text-center max-[600px]:text-xs rounded-md py-2 px-3 ">Add to Cart</button>
             </div>`
-            console.log(bookElement);
+            
             booksContainer.appendChild(bookElement);
 
         });
     }
-    let booksData = []
-document.addEventListener("DOMContentLoaded", () => {
-    booksData = JSON.parse(localStorage.getItem('booksData'));
-    console.log(booksData)
-});
+
+
+    const filterByGenre = document.getElementById('genre');
+    const filterByLang = document.getElementById('language');
+    const sorting = document.getElementById('sort');
+
+sorting.onchange = function(){
+
+    switch(sorting.value){
+        case "low": booksData.sort(function (a,b){return a.price-b.price}) ; displayBooks(booksData);break;
+        case "high": booksData.sort(function (a,b){return b.price-a.price}) ; displayBooks(booksData);break;
+        case "a": booksData.sort(function (a,b){
+        if (a.title[0] < b.title[0]) {return -1;}
+        if (a.title[0] > b.title[0]) {return 1;}
+        return 0;}) ; displayBooks(booksData);break;
+        case "z": booksData.sort(function (a,b){
+        if (a.title[0] > b.title[0]) {return -1;}
+        if (a.title[0] < b.title[0]) {return 1;}
+        return 0;}) ; displayBooks(booksData);break;
+    }
+
+}
