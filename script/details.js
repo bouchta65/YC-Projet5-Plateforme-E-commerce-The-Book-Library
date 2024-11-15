fd1bec7f94db650f209a3bf23416408f07e49a7d
safia-khoulaid <safiakhoulaid11@gmail.com>
2024-11-15 17:13:40 +0100

    fixer des mesure en design generale

--- script/details.js
+++ script/details.js
@@ -1,14 +1,14 @@
 
 let detailbook = JSON.parse(localStorage.getItem('detailbook')) || [];
 
-function affichData(){
+function affichData() {
 
 
-let table="";
+    let table = "";
 
-table=  `  
+    table = `  
    <!-- image principale -->
-            <div class=" img-hero flex bg-neutral-100	justify-center items-center content-center w-[300px] h[434px] lg:w-[500px] lg:h-[530px]">
+            <div class=" img-hero flex bg-neutral-100	justify-center items-center content-center w-[300px] h[434px] lg:w-[500px] lg:h-[530px] my-2">
                 <img src=${detailbook[0].img} id="kingImg"
                     class="w-[280px] h-[400px] lg:w-[480] lg:h-[500px]  ">
             </div>
@@ -28,13 +28,16 @@ table=  `
                     </div>
 
                     <!-- etoile -->
-                    <div class="mt-2 flex"> <span> <i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
+                    <div class="mb-2 flex"> <span> <i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
+                    <div>
                         <span> <i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
-                        <span> <i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
+                         <span> <i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
                         <span> <i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
                         <span> <i class="fa-solid fa-star" style="color: #a1a1a1;"></i></span>
+                    </div>
+
                          <!-- prix -->
-                        <pre  > (150 reviews ) | <span id="price" class="font-bold text-2xl">${detailbook[0].price}</span> | <span  id="stock" class="text-bold text-2xl ">${detailbook[0].stock}</span></pre>
+                        <pre> <span class="font-bold text-2xl">(120 views)</span> | <span id="price" class="font-bold text-2xl">${detailbook[0].price} $</span> | <span  id="stock" class="font-bold text-2xl">${detailbook[0].stock}</span></pre>
                     </div>
                    
                 </div>
@@ -54,17 +57,17 @@ table=  `
                         <div class="border-2 border-solid border-black  h-[46px] flex  rounded-md">
                             <button id="moins" class=" w-[30px] h-[40px] md:w-[40px] md:h-[44px] text-black ">-</button>
                             <button id="countour" class=" md:w-[80px] h-[44px] w-[60px] border-r-2 border-l-2 border-solid border-black text-black ">0</button>
-                            <button id="plus" class="w-[30px] h-[43px] md:w-[40px]  text-white text-center  bg-red-500 ">+</button>
+                            <button id="plus" class="w-[30px] h-[43px] md:w-[40px]  text-white text-center  rounded-tr-[3.5px]  rounded-br-[3.5px]  bg-red-500 ">+</button>
                         </div>
 
                         <!-- button de buy now -->
                         <div class="h-[45px] md:w-[165px] w-[120px] bg-red-500 border-1 border-solid border-black  text-center content center items-center rounded-md">
-                            <button id="buyNowbutton" class="rounded-sm text-center text-white h-[45px] md:w-[165px] w-[120px] ">Buy Now</button>
+                            <a href="cart.html"><button id="buyNowbutton" class="rounded-sm text-center text-white h-[45px] md:w-[165px] w-[120px] ">Buy Now</button></a>  
                         </div>
 
                         <div>
                            <button id="heartButton" class="text-black text-xl ">
-                    <i class="fa-regular fa-heart" style="color: #000000;"></i>
+                    <i class="fa-regular fa-heart fa-2x fa-2x" style="color: #000000;"></i>
                     </button>
                         </div>
 
@@ -104,7 +107,7 @@ table=  `
             </div>
 `
 
-document.querySelector("#container1").innerHTML = table
+    document.querySelector("#container1").innerHTML = table
 };
 
 
@@ -121,17 +124,17 @@ document.querySelector("#container1").innerHTML = table
 
 
 
-let quantite="";
+let quantite = "";
 
-function buyNow(){
-    document.querySelector("#buyNowbutton").addEventListener('click',()=>{
-   let quantite = countour.innerText;
-   let detailbook = JSON.parse(localStorage.getItem('detailbook')) || [];
-   let card = JSON.parse(localStorage.getItem('card')) || [];
-   detailbook[0] = { ...detailbook[0], quantity: quantite };
-    localStorage.setItem('card', JSON.stringify(detailbook));
-   console.log(detailbook)
-})
+function buyNow() {
+    document.querySelector("#buyNowbutton").addEventListener('click', () => {
+        let quantite = countour.innerText;
+        let detailbook = JSON.parse(localStorage.getItem('detailbook')) || [];
+        let card = JSON.parse(localStorage.getItem('card')) || [];
+        detailbook[0] = { ...detailbook[0], quantity: quantite };
+        localStorage.setItem('card', JSON.stringify(detailbook));
+        console.log(detailbook)
+    })
 }
 
 
@@ -143,13 +146,16 @@ affichData()
 
 
 
-let count =0;
+let count = 0;
 const moins = document.getElementById("moins");
 const plus = document.getElementById("plus");
 const countour = document.getElementById("countour");
 
 plus.addEventListener('click', () => {
-    countour.innerText = count++;
+    if (count >= 0) {
+        countour.innerText = count++;
+    }
+
 });
 
 moins.addEventListener('click', () => {
@@ -167,39 +173,39 @@ let favourite = JSON.parse(localStorage.getItem('favourite')) || [];
 
 const heartIcon = document.querySelector("#heartButton i");
 
-if(favourite.some(book => book.id === detailbook[0].id)){
+if (favourite.some(book => book.id === detailbook[0].id)) {
 
-   heartIcon.classList.add('fa-solid');
+    heartIcon.classList.add('fa-solid');
     heartIcon.classList.remove('fa-regular');
     heartIcon.style.color = 'red';
- }else{
+} else {
 
- heartIcon.classList.remove('fa-solid');
-   heartIcon.classList.add('fa-regular');
-   heartIcon.style.color = 'black'; 
- }
+    heartIcon.classList.remove('fa-solid');
+    heartIcon.classList.add('fa-regular');
+    heartIcon.style.color = 'black';
+}
 heartIcon.addEventListener('click', () => {
 
-        if (heartIcon.classList.contains('fa-solid')) {
-        
-            heartIcon.classList.remove('fa-solid');
-            heartIcon.classList.add('fa-regular');
-            heartIcon.style.color = 'black';
+    if (heartIcon.classList.contains('fa-solid')) {
 
-            const index = favourite.findIndex(book => book.id === detailbook[0].id);
-            if (index !== -1) {
-           
-                favourite.splice(index, 1); 
-            }
-        } else {
-        
-                heartIcon.classList.add('fa-solid');
-                heartIcon.classList.remove('fa-regular');
-                heartIcon.style.color = 'red';
-                favourite.push(detailbook[0]);
-               
+        heartIcon.classList.remove('fa-solid');
+        heartIcon.classList.add('fa-regular');
+        heartIcon.style.color = 'black';
 
-            }
+        const index = favourite.findIndex(book => book.id === detailbook[0].id);
+        if (index !== -1) {
 
-            localStorage.setItem('favourite', JSON.stringify(favourite));
-        });
+            favourite.splice(index, 1);
+        }
+    } else {
+
+        heartIcon.classList.add('fa-solid');
+        heartIcon.classList.remove('fa-regular');
+        heartIcon.style.color = 'red';
+        favourite.push(detailbook[0]);
+
+
+    }
+
+    localStorage.setItem('favourite', JSON.stringify(favourite));
+});
